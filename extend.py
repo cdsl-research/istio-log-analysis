346847c6cb716783a7f31134b3c3d504423e0267
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-07-14 17:36:53 +0900

    update

--- extend.py
+++ extend.py
@@ -6,11 +6,10 @@ from parser import parser
 from yaml import parse
 
 
-reqid_table = {}
-
-
 def main():
     filename = os.getenv("FILE", "v2-logs/30min.csv")
+    reqid_table = {}
+
     with open(filename) as csvfile:
         spamreader = csv.reader(csvfile, skipinitialspace=True)
         next(spamreader)  # skip header
@@ -24,16 +23,21 @@ def main():
             parsed_line = parser(log_body)
             if parsed_line is None:
                 continue
-            # elif parsed_line["UpstreamCluster"].startswith("outbound"):
-            #     continue
+            elif parsed_line["UpstreamCluster"].startswith("inbound"):
+                continue
+            elif parsed_line["Path"].startswith("/fulltext/_search"):
+                continue
+
+            # parsed_line["EndpointMethod"] = "GET"
+            # parsed_line["EndpointPath"] = "/"
 
             reqid = parsed_line["ReqId"]
             reqid_lst = reqid_table.get(reqid, [])
             reqid_lst.append(parsed_line)
             reqid_table[reqid] = reqid_lst
 
-            import json
-            print(json.dumps(reqid_table, indent=4))
+    import json
+    print(json.dumps(reqid_table, indent=4))
 
 
 if __name__ == "__main__":
