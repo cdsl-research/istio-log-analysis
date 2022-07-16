99d88ba7a3ccdc90a097e204587f28cb52b57a4f
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-07-16 11:26:07 +0900

    merged parser and sub

--- parser.py
+++ parser.py
@@ -32,3 +32,14 @@ def parser(raw_text: str):
     if matched is None:
         return None
     return matched.groupdict()
+
+
+def main():
+    import json
+
+    raw_txt = """[2022-05-25T06:38:13.956Z] "GET / HTTP/1.1" 200 - via_upstream - "-" 0 35430 6 6 "-" "Python/3.9 aiohttp/3.8.1" "e4c5583b-565c-9026-8e5d-974bdf8ab467" "service2.prod:4000" "10.42.2.204:4000" inbound|4000|| 127.0.0.6:56089 10.42.2.204:4000 10.42.3.252:54568 - default "GET /" service2(200)|"""
+    # raw_txt = """[2022-05-25T06:38:13.956Z] "GET / HTTP/1.1" 200 - via_upstream - "-" 0 35430 6 6 "-" "Python/3.9 aiohttp/3.8.1" "e4c5583b-565c-9026-8e5d-974bdf8ab467" "service2.prod:4000" "10.42.2.204:4000" inbound|4000|| 127.0.0.6:56089 10.42.2.204:4000 10.42.3.252:54568 - default "GET /" service2(200)|service3(200)|"""
+    print(raw_txt)
+
+    result = parser(raw_txt)
+    print(json.dumps(result, indent=2))
