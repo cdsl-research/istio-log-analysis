0613c86ec67e9307be93d28ae16896780cd946c8
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-07-17 16:11:55 +0900

    update runner

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,8 +1,19 @@
+
+
 task_extend() {
-    filelist=$(ls v2-logs/*.csv)
+    filelist=$(ls v2-logs/*.csv | grep -v ext)
     for f in $filelist
     do
         echo "Extend: $f"
         FILE=$f python extend.py
+    done
+}
+
+task_summary() {
+    filelist=$(ls v2-logs/*-ext.csv)
+    for f in $filelist
+    do
+        echo "Summary: $f"
+        FILE=$f python csv_analysis_v2.py
     done
 }
