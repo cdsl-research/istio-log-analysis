45f5a8c670770f25ea02936ab751f45f08a14411
Tomoyuki Koyama <g21210247f@edu.teu.ac.jp>
2022-11-12 03:09:24 +0000

    fix parser

--- logs-v3/parser.py
+++ logs-v3/parser.py
@@ -7,7 +7,7 @@ import json
 match_pattern = (
     # '[2022-05-12T00:57:09.548Z]'
     r"\[(?P<DateTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)]"
-    r' "(?P<Method>\w+)'  # 'GET'
+    r' "(?P<Method>[^ ]+)'  # 'GET'
     r" (?P<Path>[^ ]+)"  # '/author'
     r' (?P<Protocol>[^ ]+)"'  # 'HTTP/1.1'
     r" (?P<Status>\d{,3})"  # '200'
@@ -42,13 +42,15 @@ def message_parser(raw_text: str):
     raw_text = raw_text.replace("\n", "")
     matched = re.match(match_pattern, raw_text)
     if matched is None:
+        print("Err", raw_text)
         return None
     return matched.groupdict()
 
 
 def test():
-    raw_txt = """[2022-05-25T06:38:13.956Z] "GET / HTTP/1.1" 200 - via_upstream - "-" 0 35430 6 6 "-" "Python/3.9 aiohttp/3.8.1" "e4c5583b-565c-9026-8e5d-974bdf8ab467" "service2.prod:4000" "10.42.2.204:4000" inbound|4000|| 127.0.0.6:56089 10.42.2.204:4000 10.42.3.252:54568 - default "GET /" service2(200)|"""
+    # raw_txt = """[2022-05-25T06:38:13.956Z] "GET / HTTP/1.1" 200 - via_upstream - "-" 0 35430 6 6 "-" "Python/3.9 aiohttp/3.8.1" "e4c5583b-565c-9026-8e5d-974bdf8ab467" "service2.prod:4000" "10.42.2.204:4000" inbound|4000|| 127.0.0.6:56089 10.42.2.204:4000 10.42.3.252:54568 - default "GET /" service2(200)|"""
     # raw_txt = """[2022-05-25T06:38:13.956Z] "GET / HTTP/1.1" 200 - via_upstream - "-" 0 35430 6 6 "-" "Python/3.9 aiohttp/3.8.1" "e4c5583b-565c-9026-8e5d-974bdf8ab467" "service2.prod:4000" "10.42.2.204:4000" inbound|4000|| 127.0.0.6:56089 10.42.2.204:4000 10.42.3.252:54568 - default "GET /" service2(200)|service3(200)|"""
+    raw_txt = """[2022-11-10T07:53:51.283Z] "- - -" 0 - - - "-" 3505 2337 5051 - "-" "-" "-" "-" "10.42.0.254:8000" inbound|8000|| 127.0.0.6:48119 10.42.0.254:8000 10.42.0.247:56434 outbound_.4000_._.front-app.front.svc.cluster.local -"""
     print(raw_txt)
 
     result = message_parser(raw_txt)
